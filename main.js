8114f022dd2d21e9a7a775c4df8477f869d92e3e
tsl <taosiliang1127tsl@163.com>
2018-01-25 22:28:08 +0800

    代码优化1，封装函数

--- main.js
+++ main.js
@@ -1,51 +1,58 @@
 let $buttons = $('#buttonWrapper>button')
 let $slides = $('#slides')
-let $images = $slides.children('img')
-let $firstCopy = $images.eq(0).clone(true)
-// console.log($firstCopy[0].outerHTML)
-let $lastCopy = $images.eq($images.length-1).clone(true)
-// console.log($lastCopy[0].outerHTML)
-$slides.append($firstCopy)  //把第一张放到最后去
-$slides.prepend($lastCopy)  //把最后一张放到最前面  
-
-$slides.css({transform:'translateX(-400px)'})   //让1在当前窗口
-
-
+let $images = $slides.children('img') 
 let current = 0
 
-$buttons.eq(0).on('click',function(){
-    if(current ===2){
-        console.log('说明你是从最后一张到第一张')
-        $slides.css({transform:'translateX(-1600px)'})  
-        .one('transitionend',function(){
-            $slides.hide()
-            .offset()
-            $slides.css({transform:'translateX(-400px)'}) //要先隐藏再做动画，不然会被用户看穿
-            .show()
-        })
-    }else{
-        $slides.css({transform:'translateX(-400px)'})       
-    }
-    current =0
-})
+makeFakeSlides()
+$slides.css({transform:'translateX(-400px)'})   //让1在当前窗口
+bindEvents()
 
-$buttons.eq(1).on('click',function(){
-        $slides.css({transform:'translateX(-800px)'})
-        current=1
-})
+function bindEvents(){
+    $buttons.eq(0).on('click',function(){
+        if(current ===2){
+            console.log('说明你是从最后一张到第一张')
+            $slides.css({transform:'translateX(-1600px)'})  
+            .one('transitionend',function(){
+                $slides.hide()
+                .offset()
+                $slides.css({transform:'translateX(-400px)'}) //要先隐藏再做动画，不然会被用户看穿
+                .show()
+            })
+        }else{
+            $slides.css({transform:'translateX(-400px)'})       
+        }
+        current =0
+    })
+    
+    $buttons.eq(1).on('click',function(){
+            $slides.css({transform:'translateX(-800px)'})
+            current=1
+    })
+    
+    $buttons.eq(2).on('click',function(){
+        if(current===0){
+            console.log('说明你是从第一张到最后一张')
+            $slides.css({transform:'translateX(0px)'})  
+            .one('transitionend',function(){
+                $slides.hide()
+                .offset()
+                $slides.css({transform:'translateX(-1200px)'}) //要先隐藏再做动画，不然会被用户看穿
+                .show()
+            })    
+        }else{
+            $slides.css({transform:'translateX(-1200px)'})
+        }
+        current =2
+    })
+}
 
-$buttons.eq(2).on('click',function(){
-    if(current===0){
-        console.log('说明你是从第一张到最后一张')
-        $slides.css({transform:'translateX(0px)'})  
-        .one('transitionend',function(){
-            $slides.hide()
-            .offset()
-            $slides.css({transform:'translateX(-1200px)'}) //要先隐藏再做动画，不然会被用户看穿
-            .show()
-        })    
-    }else{
-        $slides.css({transform:'translateX(-1200px)'})
-    }
-    current =2
-})
+
+
+function makeFakeSlides(){
+    let $firstCopy = $images.eq(0).clone(true)
+    // console.log($firstCopy[0].outerHTML)
+    let $lastCopy = $images.eq($images.length-1).clone(true)
+    // console.log($lastCopy[0].outerHTML)
+    $slides.append($firstCopy)  //把第一张放到最后去
+    $slides.prepend($lastCopy)  //把最后一张放到最前面 
+}
