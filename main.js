815f48f27b208de9bc920ec62e9f6208168753d6
tsl <taosiliang1127tsl@163.com>
2018-01-25 17:28:32 +0800

    解决切换页面后（再切回来），页面乱掉的问题

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let n
 初始化 ()
-setInterval(()=> {   
+let timer = setInterval(()=> {   
     makeLeave(getImage(n))  //
     .one('transitionend',(e)=>{
         makeEnter($(e.currentTarget))
@@ -8,6 +8,22 @@ setInterval(()=> {
     makeCurrent(getImage(n+1))  
     n += 1 
 }, 3000);
+
+document.addEventListener('visibilitychange',function(e){
+    // console.log(document.hidden)
+    if(document.hidden){
+        window.clearInterval(timer)  //把闹钟干掉
+    }else{
+        timer = setInterval(()=> {   
+            makeLeave(getImage(n))  //
+            .one('transitionend',(e)=>{
+                makeEnter($(e.currentTarget))
+            })
+            makeCurrent(getImage(n+1))  
+            n += 1 
+        }, 3000);
+    }
+})
 
 
 //下面为封装的函数
